28d1a04f451c743d79cac08367a8ba52d43bc5ef
Juan Senen <juansenen@hotmail.com>
2023-03-26 19:56:55 +0200

    Set department name on Table, but only one column

--- src/main/java/com/juansenen/PSPApiMuseum/controller/AppController.java
+++ src/main/java/com/juansenen/PSPApiMuseum/controller/AppController.java
@@ -1,12 +1,13 @@
 package com.juansenen.PSPApiMuseum.controller;
 
+import com.juansenen.PSPApiMuseum.domain.Deparments;
 import com.juansenen.PSPApiMuseum.domain.Department;
-import com.juansenen.PSPApiMuseum.domain.ObjectsByID;
 import com.juansenen.PSPApiMuseum.domain.ObjectsMain;
 import com.juansenen.PSPApiMuseum.service.MetService;
 import com.juansenen.PSPApiMuseum.task.TotalObjectTask;
 import io.reactivex.functions.Consumer;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,6 +16,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class AppController implements Initializable {
@@ -26,14 +28,26 @@ public class AppController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        prepareTableDepartment();
+
+        MetService service = new MetService();
+        Consumer<Deparments> dep = (info) -> {
+            List<Department> departmentsList = info.getDepartments();
+            tableMain.setItems(FXCollections.observableArrayList(departmentsList));
+        };
+        service.getAllDeparments().subscribe(dep);
+
 
     }
 
-    public void prepareTableODepartment(){
+    public void prepareTableDepartment(){
         TableColumn<Department, Integer> idColumn = new TableColumn<>("Id");
         idColumn.setCellValueFactory(new PropertyValueFactory<>("departmentId"));
+        TableColumn<Department, String> nameColumn = new TableColumn<>("Nombre");
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("displayName"));
 
         tableMain.getColumns().add(idColumn);
+        tableMain.getColumns().add(nameColumn);
 
     }
 
