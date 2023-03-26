10988139e8cee617aa34401f1451e8c107d2e766
Juan Senen <juansenen@hotmail.com>
2023-03-26 16:53:58 +0200

    Adding Table

--- src/main/java/com/juansenen/PSPApiMuseum/controller/AppController.java
+++ src/main/java/com/juansenen/PSPApiMuseum/controller/AppController.java
@@ -1,10 +1,17 @@
 package com.juansenen.PSPApiMuseum.controller;
 
+import com.juansenen.PSPApiMuseum.domain.ObjectsByID;
 import com.juansenen.PSPApiMuseum.domain.ObjectsMain;
 import com.juansenen.PSPApiMuseum.service.MetService;
 import io.reactivex.functions.Consumer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,6 +19,8 @@ import java.util.ResourceBundle;
 public class AppController implements Initializable {
     public Label labelTotal;
     public Label labelMessage;
+    public TableView<ObjectsByID> tableObjects;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -26,6 +35,16 @@ public class AppController implements Initializable {
         };
 
         metService.getTotalObjects().subscribe(totalObj);
+
+
+    }
+
+    public void prepareTableObjects(){
+        TableColumn<ObjectsByID, String> idColumn = new TableColumn<>("Id");
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("objectID"));
+        tableObjects.getColumns().add(idColumn);
+
+
 
 
     }
