7d9eb6f059a91a03923efd732598e3ca7f561de9
Juan Senen <juansenen@hotmail.com>
2023-03-26 15:55:26 +0200

    Inicio

---
+++ src/main/java/com/juansenen/PSPApiMuseum/App.java
@@ -0,0 +1,35 @@
+package com.juansenen.PSPApiMuseum;
+
+import com.juansenen.PSPApiMuseum.controller.AppController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        //Cargamos el controlador de la pantalla incial de Splash
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/PSPApiMuseum.fxml"));
+        loader.setController(new AppController());
+        AnchorPane anchorPane = loader.load();
+
+        //Dibujamos la pantalla
+        Scene scene = new Scene(anchorPane);
+        stage.setScene(scene);
+        stage.setTitle("Museum");
+        stage.show();
+    }
+    public static void main(String[] args) {launch();
+
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+}
---
+++ src/main/java/com/juansenen/PSPApiMuseum/controller/AppController.java
@@ -0,0 +1,17 @@
+package com.juansenen.PSPApiMuseum.controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AppController implements Initializable {
+    private Label labelTotal;
+    private Label labelMessage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
