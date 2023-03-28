f38a5f6e9d30ed5a24afc0342b639827f55fb145
Juan Senen <juansenen@hotmail.com>
2023-03-28 21:53:12 +0200

    Comentarios Codigo

--- src/main/java/com/juansenen/PSPApiMuseum/service/MetAPI.java
+++ src/main/java/com/juansenen/PSPApiMuseum/service/MetAPI.java
@@ -1,7 +1,6 @@
 package com.juansenen.PSPApiMuseum.service;
 
 import com.juansenen.PSPApiMuseum.domain.Deparments;
-import com.juansenen.PSPApiMuseum.domain.ObjectsList;
 import com.juansenen.PSPApiMuseum.domain.ObjectsMain;
 import com.juansenen.PSPApiMuseum.domain.ObjectsByID;
 import io.reactivex.Observable;
@@ -15,14 +14,14 @@ public interface MetAPI {
     /** interfaz de Retrofit que defina los endpoints de la API */
 
     @GET("objects/{objectId}")
-    Observable<ObjectsByID> loadOneObject(@Path("objectId") int objectId);
+    Observable<ObjectsByID> loadOneObject(@Path("objectId") int objectId);  /** Busqueda de todos los datos de un objeto por su Id */
 
     @GET("objects")
-    Observable<ObjectsMain> loadObjects();    /** Obtener objetos */
+    Observable<ObjectsMain> loadObjects();    /** Obtener listado objetos Total y Ids todos los objetos */
     @GET("departments")
-    Observable<Deparments> loadDeparments(); /** Obtener los departamentos*/
+    Observable<Deparments> loadDeparments(); /** Obtener los departamentos con Id y Nombre*/
 
-    @GET("search") /** Obtener los objetos totales de un Departamento**/
+    @GET("search") /** Obtener datos de los objetos con diferentes filtros de busqueda **/
     Observable<ObjectsMain> loadObjectsByDepartment(@Query("departmentIds") int departmentIds, @Query("q") String cadena);
 
 
